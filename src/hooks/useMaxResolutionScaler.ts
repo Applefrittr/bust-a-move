eff2a6b83bc99a0c9f65e5825954b16f780ccb5d
applefrittr <jacob.lee.jordan@gmail.com>
2025-03-06 16:00:30 -0500

    tweak to useMaxResolutionScaler -> useState intialized by running an anonymous fn to check screen resolution

--- src/hooks/useMaxResolutionScaler.ts
+++ src/hooks/useMaxResolutionScaler.ts
@@ -2,7 +2,11 @@ import { useState, useEffect } from "react";
 import { MAXRESOLUTIONSCALER } from "../engine/utils/constantValues";
 
 export default function useMaxResolutionScaler(width: number) {
-  const [scaler, setScaler] = useState(MAXRESOLUTIONSCALER.HD);
+  const [scaler, setScaler] = useState(() => {
+    if (width < 2000) return MAXRESOLUTIONSCALER.HD;
+    else if (width >= 2000 && width < 3000) return MAXRESOLUTIONSCALER["2K"];
+    else return MAXRESOLUTIONSCALER["4K"];
+  });
 
   useEffect(() => {
     if (width < 2000) setScaler(MAXRESOLUTIONSCALER.HD);
