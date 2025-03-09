0b98b523ae42ea480ff5d418c819a5942d50620c
applefrittr <jacob.lee.jordan@gmail.com>
2025-03-09 03:24:11 -0400

    adjustOrbPositioning refactored to use atan2 instead of atan, angle postioning ranges adjusted

--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -116,7 +116,7 @@ export default class Game {
   start() {
     window.addEventListener("keydown", this.keyDownEvent);
     window.addEventListener("keyup", this.keyUpEvent);
-    generateLevel(this, 15, 0.05);
+    generateLevel(this, 5, 0.05);
     this.animationLoop();
   }
 
