0dac694bf73ef4714761513262d4697ce75e53de
applefrittr <jacob.lee.jordan@gmail.com>
2025-03-15 20:47:55 -0400

    layout tweaks, mutation, and state updates to GameOverModal

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -1,7 +1,7 @@
 import Game from "../engine/Game";
 import LinkBtn from "./LinkBtn";
 import UIBtn from "./UIBtn";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getScores, submitScore, FBUserDoc } from "../services/queries";
 
 type GameOverModalProps = {
@@ -16,14 +16,18 @@ export default function GameOverModal({
   syncReactFrames,
 }: GameOverModalProps) {
   cancelAnimationFrame(frame);
+  const queryClient = useQueryClient();
 
   const { data } = useQuery({
     queryKey: ["scores"],
     queryFn: getScores,
   });
 
-  const { mutate, isSuccess, isPending, isError } = useMutation({
+  const { mutate, isSuccess, isPending, isError, isIdle } = useMutation({
     mutationFn: submitScore,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["scores"] });
+    },
   });
 
   const submitNewScore = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -43,7 +47,7 @@ export default function GameOverModal({
       {data &&
         (game.score > data[data.length - 1].score || data.length < 10) && (
           <form
-            className="bg-black text-white p-4 border-2 border-white rounded-2xl w-max absolute top-1/4 -translate-y-1/4 flex flex-col justify-center items-center gap-4"
+            className="bg-black text-white p-4 border-2 border-white rounded-2xl w-max absolute top-1/4 -translate-y-1/4 flex flex-col justify-center items-center gap-2"
             onSubmit={submitNewScore}
           >
             <div className="flex gap-2 items-center">
@@ -56,17 +60,24 @@ export default function GameOverModal({
               placeholder="name"
               className="bg-white text-black text-center"
               size={15}
+              minLength={3}
+              maxLength={15}
               name="name"
             />
-            <div className="m-auto w-max">
-              <UIBtn>SUBMIT</UIBtn>
-            </div>
-            {isSuccess && <p>SUCCESS!</p>}
+            <p className="text-[.5rem]">3-15 characters long</p>
+            {isIdle && (
+              <div className={`m-auto w-max`}>
+                <UIBtn>SUBMIT</UIBtn>
+              </div>
+            )}
+            {isSuccess && (
+              <p className="text-green-300">SCORE ADDED SUCCESSFULLY</p>
+            )}
             {isPending && <p>SUBMITTING...</p>}
-            {isError && <p>Something went wrong</p>}
+            {isError && <p className="text-red-300">Something went wrong</p>}
           </form>
         )}
-      <div className="flex gap-8 justify-center m-4 absolute bottom-3/8">
+      <div className="flex gap-8 justify-center m-4 translate-y-full">
         <UIBtn
           cb={() => {
             game.restart();
