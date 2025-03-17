f3291961505823f5043ac648da0ca9cdb994e414
applefrittr <jacob.lee.jordan@gmail.com>
2025-03-16 21:25:06 -0400

    Scoreboard now tracks highest round

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -2,7 +2,12 @@ import Game from "../engine/Game";
 import LinkBtn from "./LinkBtn";
 import UIBtn from "./UIBtn";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getScores, submitScore, FBUserDoc } from "../services/queries";
+import {
+  getScores,
+  submitScore,
+  FBUserDoc,
+  getRounds,
+} from "../services/queries";
 import {
   RegExpMatcher,
   englishDataset,
@@ -29,15 +34,20 @@ export default function GameOverModal({
   });
   const [profanity, setProfanity] = useState(false);
 
-  const { data } = useQuery({
+  const scores = useQuery({
     queryKey: ["scores"],
     queryFn: getScores,
+  });
+
+  const rounds = useQuery({
+    queryKey: ["rounds"],
+    queryFn: getRounds,
   });
 
   const { mutate, isSuccess, isPending, isError, isIdle } = useMutation({
     mutationFn: submitScore,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["scores"] });
+      queryClient.invalidateQueries({ queryKey: ["scores", "rounds"] });
     },
   });
 
@@ -45,10 +55,11 @@ export default function GameOverModal({
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
 
-    const userData: FBUserDoc = { name: "", score: 0 };
+    const userData: FBUserDoc = { name: "", score: 0, round: 0 };
 
     userData.name = formData.get("name") as string;
     userData.score = game.score;
+    userData.round = game.round;
 
     if (matcher.hasMatch(userData.name)) {
       setProfanity(true);
@@ -60,45 +71,51 @@ export default function GameOverModal({
 
   return (
     <section className="absolute left-0 top-0 h-full w-full z-10 flex flex-col justify-center items-center gap-8">
-      {data &&
-        (game.score > data[data.length - 1].score || data.length < 10) && (
-          <form
-            className="bg-black text-white p-4 border-2 border-white rounded-2xl w-max absolute top-1/4 -translate-y-1/4 flex flex-col justify-center items-center gap-2"
-            onSubmit={submitNewScore}
-          >
-            <div className="flex gap-2 items-center">
-              <div className="w-6 h-6 bg-[url(/src/assets/stars.png)] bg-cover animate-star"></div>
-              <h2 className="w-max">NEW HIGH SCORE</h2>
-              <div className="w-6 h-6 bg-[url(/src/assets/stars.png)] bg-cover animate-star"></div>
-            </div>
-            <input
-              type="text"
-              placeholder="name"
-              className={`text-black text-center ${
-                profanity ? "bg-red-300" : "bg-white"
-              }`}
-              size={15}
-              minLength={3}
-              maxLength={15}
-              name="name"
-              onFocus={() => setProfanity(false)}
-            />
-            {profanity && (
-              <p className="text-[.5rem] text-red-300">Really???</p>
-            )}
-            <p className="text-[.5rem]">3-15 characters long</p>
-            {isIdle && (
-              <div className={`m-auto w-max`}>
-                <UIBtn>SUBMIT</UIBtn>
+      {(scores.data &&
+        (scores.data.length === 0 ||
+          game.score > scores.data[scores.data.length - 1].score ||
+          scores.data.length < 10)) ||
+        (rounds.data &&
+          (rounds.data.length === 0 ||
+            game.round > rounds.data[rounds.data.length - 1].score ||
+            rounds.data.length < 10) && (
+            <form
+              className="bg-black text-white p-4 border-2 border-white rounded-2xl w-max absolute top-1/4 -translate-y-1/4 flex flex-col justify-center items-center gap-2"
+              onSubmit={submitNewScore}
+            >
+              <div className="flex gap-2 items-center">
+                <div className="w-6 h-6 bg-[url(/src/assets/stars.png)] bg-cover animate-star"></div>
+                <h2 className="w-max">NEW LEADERBOARD PLACEMENT</h2>
+                <div className="w-6 h-6 bg-[url(/src/assets/stars.png)] bg-cover animate-star"></div>
               </div>
-            )}
-            {isSuccess && (
-              <p className="text-green-300">SCORE ADDED SUCCESSFULLY</p>
-            )}
-            {isPending && <p>SUBMITTING...</p>}
-            {isError && <p className="text-red-300">Something went wrong</p>}
-          </form>
-        )}
+              <input
+                type="text"
+                placeholder="name"
+                className={`text-black text-center ${
+                  profanity ? "bg-red-300" : "bg-white"
+                }`}
+                size={15}
+                minLength={3}
+                maxLength={15}
+                name="name"
+                onFocus={() => setProfanity(false)}
+              />
+              {profanity && (
+                <p className="text-[.5rem] text-red-300">Really???</p>
+              )}
+              <p className="text-[.5rem]">3-15 characters long</p>
+              {isIdle && (
+                <div className={`m-auto w-max`}>
+                  <UIBtn>SUBMIT</UIBtn>
+                </div>
+              )}
+              {isSuccess && (
+                <p className="text-green-300">SCORE ADDED SUCCESSFULLY</p>
+              )}
+              {isPending && <p>SUBMITTING...</p>}
+              {isError && <p className="text-red-300">Something went wrong</p>}
+            </form>
+          ))}
       <div className="flex gap-8 justify-center m-4 translate-y-full">
         <UIBtn
           cb={() => {
