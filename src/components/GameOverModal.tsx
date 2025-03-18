e3993ed86d002b259356156312db3436fc9a9750
applefrittr <jacob.lee.jordan@gmail.com>
2025-03-18 17:07:31 -0400

    bug fix in conditional render statement in GameOverModal

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -71,51 +71,49 @@ export default function GameOverModal({
 
   return (
     <section className="absolute left-0 top-0 h-full w-full z-10 flex flex-col justify-center items-center gap-8">
-      {(scores.data &&
+      {((scores.data &&
         (scores.data.length === 0 ||
           game.score > scores.data[scores.data.length - 1].score ||
           scores.data.length < 10)) ||
         (rounds.data &&
           (rounds.data.length === 0 ||
             game.round > rounds.data[rounds.data.length - 1].score ||
-            rounds.data.length < 10) && (
-            <form
-              className="bg-black text-white p-4 border-2 border-white rounded-2xl w-max absolute top-1/4 -translate-y-1/3 flex flex-col justify-center items-center gap-2"
-              onSubmit={submitNewScore}
-            >
-              <div className="flex gap-2 items-center">
-                <div className="w-6 h-6 bg-[url(/src/assets/stars.png)] bg-cover animate-star"></div>
-                <h2 className="w-max">NEW PLACEMENT</h2>
-                <div className="w-6 h-6 bg-[url(/src/assets/stars.png)] bg-cover animate-star"></div>
-              </div>
-              <input
-                type="text"
-                placeholder="name"
-                className={`text-black text-center ${
-                  profanity ? "bg-red-300" : "bg-white"
-                }`}
-                size={15}
-                minLength={3}
-                maxLength={15}
-                name="name"
-                onFocus={() => setProfanity(false)}
-              />
-              {profanity && (
-                <p className="text-[.5rem] text-red-300">Really???</p>
-              )}
-              <p className="text-[.5rem]">3-15 characters long</p>
-              {isIdle && (
-                <div className={`m-auto w-max`}>
-                  <UIBtn>SUBMIT</UIBtn>
-                </div>
-              )}
-              {isSuccess && (
-                <p className="text-green-300">SCORE ADDED SUCCESSFULLY</p>
-              )}
-              {isPending && <p>SUBMITTING...</p>}
-              {isError && <p className="text-red-300">Something went wrong</p>}
-            </form>
-          ))}
+            rounds.data.length < 10))) && (
+        <form
+          className="bg-black text-white p-4 border-2 border-white rounded-2xl w-max absolute top-1/4 -translate-y-1/3 flex flex-col justify-center items-center gap-2"
+          onSubmit={submitNewScore}
+        >
+          <div className="flex gap-2 items-center">
+            <div className="w-6 h-6 bg-[url(/src/assets/stars.png)] bg-cover animate-star"></div>
+            <h2 className="w-max">NEW PLACEMENT</h2>
+            <div className="w-6 h-6 bg-[url(/src/assets/stars.png)] bg-cover animate-star"></div>
+          </div>
+          <input
+            type="text"
+            placeholder="name"
+            className={`text-black text-center ${
+              profanity ? "bg-red-300" : "bg-white"
+            }`}
+            size={15}
+            minLength={3}
+            maxLength={15}
+            name="name"
+            onFocus={() => setProfanity(false)}
+          />
+          {profanity && <p className="text-[.5rem] text-red-300">Really???</p>}
+          <p className="text-[.5rem]">3-15 characters long</p>
+          {isIdle && (
+            <div className={`m-auto w-max`}>
+              <UIBtn>SUBMIT</UIBtn>
+            </div>
+          )}
+          {isSuccess && (
+            <p className="text-green-300">SCORE ADDED SUCCESSFULLY</p>
+          )}
+          {isPending && <p>SUBMITTING...</p>}
+          {isError && <p className="text-red-300">Something went wrong</p>}
+        </form>
+      )}
       <div className="flex gap-8 justify-center m-4 translate-y-full">
         <UIBtn
           cb={() => {
