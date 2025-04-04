e50dda29256c02a0cef7269dc551b59fd8893b92
applefrittr <jacob.lee.jordan@gmail.com>
2025-04-04 00:03:21 -0400

    fix responsive breakpoints on Homes.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,19 +8,19 @@ export default function Home() {
         <img
           src={Title}
           alt="Bust a Move"
-          className="min-w-3xs w-full h-auto animate-pulse p-4 flex-auto"
+          className="min-w-xs w-full h-auto animate-pulse p-6 flex-auto"
         />
-        <div className="animate-slide absolute -top-16">
+        <div className="animate-slide absolute -top-14">
           <div className="w-[98px] h-[98px] bg-[url(/src/assets/title-run.png)] bg-cover animate-run"></div>
         </div>
-        <div className="flex flex-col md:flex-row gap-2 relative m-auto w-max">
+        <div className="flex flex-col sm:flex-row gap-2 relative m-auto w-max">
           <LinkBtn to={"game"} text={"START"} />
           <LinkBtn to={"options"} text={"OPTIONS"} />
           <LinkBtn to={"scoreboard"} text={"SCOREBOARD"} />
-          <div className="w-[98px] h-[98px] bg-[url(/src/assets/title-look.png)] bg-cover animate-look absolute top-36 right-1/12 md:-top-1/2 md:-right-1/4"></div>
+          <div className="w-[98px] h-[98px] bg-[url(/src/assets/title-look.png)] bg-cover animate-look absolute top-36 right-1/12 sm:-top-1/2 sm:-right-1/4"></div>
         </div>
       </section>
-      <p className="text-[.50rem] sm:text-[.70rem] p-2 fixed bottom-0 text-center">
+      <p className="text-[.50rem] md:text-[.70rem] p-2 fixed bottom-0 text-center">
         &copy;Taito Re-created with Web Technology by Applefrittr 2025
       </p>
     </main>
