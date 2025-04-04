997822fa85258b1127f59b3194ea09ec2ea4a6cd
applefrittr <jacob.lee.jordan@gmail.com>
2025-04-03 20:22:45 -0400

    tweaks to hero in Home.tsx -> reposnive design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,20 +4,20 @@ import Title from "../assets/title.png";
 export default function Home() {
   return (
     <main className="h-dvh flex items-center justify-center flex-col bg-[url(/src/assets/title-bg.png)] bg-repeat overflow-x-hidden">
-      <section className="relative">
+      <section className="relative min-w-min w-1/2 max-w-3xl flex flex-col items-center">
         <img
           src={Title}
           alt="Bust a Move"
-          className="m-auto w-96 lg:w-4xl h-auto animate-pulse p-8"
+          className="min-w-3xs w-full h-auto animate-pulse p-4 flex-auto"
         />
-        <div className="animate-slide absolute -top-12">
+        <div className="animate-slide absolute -top-16">
           <div className="w-[98px] h-[98px] bg-[url(/src/assets/title-run.png)] bg-cover animate-run"></div>
         </div>
-        <div className="flex flex-col sm:flex-row gap-2 relative m-auto w-max">
+        <div className="flex flex-col md:flex-row gap-2 relative m-auto w-max">
           <LinkBtn to={"game"} text={"START"} />
           <LinkBtn to={"options"} text={"OPTIONS"} />
           <LinkBtn to={"scoreboard"} text={"SCOREBOARD"} />
-          <div className="w-[98px] h-[98px] bg-[url(/src/assets/title-look.png)] bg-cover animate-look absolute top-36 right-1/12 sm:-top-1/2 sm:-right-1/4"></div>
+          <div className="w-[98px] h-[98px] bg-[url(/src/assets/title-look.png)] bg-cover animate-look absolute top-36 right-1/12 md:-top-1/2 md:-right-1/4"></div>
         </div>
       </section>
       <p className="text-[.50rem] sm:text-[.70rem] p-2 fixed bottom-0 text-center">
